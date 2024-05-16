fb8938148e0eb5f757bfeaf3f8e2976a9ee257e8
Daniel Levie <daniel.levie@afry.com>
2024-05-16 15:33:52 +0100

    remove broken reference to Cython.Build  cythonize
    This was causing installs to fail.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
-from Cython.Build import cythonize
+# from Cython.Build import cythonize
 
 setup(name='coad',
       version='2.0',
       description='Class-Object-Attribute Data manipution for Plexos',
       author='Harry Sorensen',
       author_email='harry.sorensen@nrel.gov',
-      packages=['coad'],
-      ext_modules=cythonize("coad/compress_interval.pyx")
+      packages=['coad']
+      # ext_modules=cythonize("coad/compress_interval.pyx")
     )
