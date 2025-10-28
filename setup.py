1b20b3f6c989c46c3c4b7ffd46d97762691c666a
Measrainsey Meng <measrainsey@pm.me>
2025-10-28 15:55:08 +0100

    build: convert `setup.py` into minimalist setuptools shim and use `pyproject.toml`

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
-from distutils.core import setup
-# from Cython.Build import cythonize
+from setuptools import setup
 
-setup(name='coad',
-      version='2.0',
-      description='Class-Object-Attribute Data manipution for Plexos',
-      author='Harry Sorensen',
-      author_email='harry.sorensen@nrel.gov',
-      packages=['coad']
-      # ext_modules=cythonize("coad/compress_interval.pyx")
-    )
+
+if __name__ == "__main__":
+    setup()
